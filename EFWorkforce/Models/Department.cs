57ea44e6790071c68ea8e26989008a0c046fc747
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2020-01-20 14:49:38 -0600

    scaffold employee controller and view

---
+++ EFWorkforce/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFWorkforce.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public int Budget { get; set; }
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
---
+++ EFWorkforce/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFWorkforce.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { get; set; }
+
+        [Display(Name = "Is Supervisor")]
+        public bool IsSupervisor { get; set; }
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+        public int ComputerId { get; set; }
+        public Computer Computer { get; set; }
+
+        public string Email { get; set; }
+    }
+}
